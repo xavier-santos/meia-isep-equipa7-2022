9893cd624436f5506502331939bca074b5341ae8
Joaquim Martins <jasm@ispgaya.pt>
2022-10-19 22:19:47 +0100

    Versão inicial

---
+++ Haemorrhage-How/src/main/java/org/engcia/model/Hypothesis.java
@@ -0,0 +1,26 @@
+package org.engcia.model;
+
+import org.engcia.haemorrhage.Haemorrhage;
+
+public class Hypothesis extends Fact{
+    private String description;
+    private String value;
+
+    public Hypothesis(String description, String value) {
+        this.description = description;
+        this.value = value;
+        Haemorrhage.agendaEventListener.addRhs(this);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String toString() {
+        return (description + " = " + value);
+    }
+}
